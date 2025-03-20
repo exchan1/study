408dbbf75f4f5aa50ce6d39f934c0023471efb27
dkkim <exchan1@gmail.com>
2025-03-21 00:59:35 +0900

    lotto 추가

--- node/lotto-3.js
+++ node/lotto-3.js
@@ -1,12 +1,15 @@
-function generateLottoNumbers() {
-  const allNumbers = new Set(); // 전체 생성된 숫자를 저장하는 Set
+function generateUniqueLottoNumbers() {
+  const allUsedNumbers = new Set(); // 이미 사용된 모든 숫자를 저장하는 Set
 
   for (let i = 0; i < 5; i++) {
     const numbers = new Set(); // 각 회차별 숫자를 저장하는 Set
 
     while (numbers.size < 6) {
       const randomNumber = Math.floor(Math.random() * 45) + 1;
-      numbers.add(randomNumber);
+      if (!allUsedNumbers.has(randomNumber)) {
+        numbers.add(randomNumber);
+        allUsedNumbers.add(randomNumber);
+      }
     }
 
     const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
@@ -14,24 +17,13 @@ function generateLottoNumbers() {
       return number < 10 ? "0" + number : String(number);
     });
 
-    // 이미 나온 숫자인지 확인하고, 아니라면 전체 Set에 추가하고 출력
-    if (!hasDuplicate(allNumbers, formattedNumbers)) {
-      formattedNumbers.forEach((num) => allNumbers.add(num));
-      console.log(formattedNumbers);
-    } else {
-      i--; // 중복 발생 시 다시 시도
-    }
+    console.log(formattedNumbers);
+  }
+
+  // 만약 30개의 고유한 숫자를 생성하지 못한 경우 (극히 드문 경우)
+  if (allUsedNumbers.size < 30) {
+    console.warn("경고: 30개의 고유한 숫자를 생성하지 못했습니다.");
   }
 }
 
-// 중복 확인 함수
-function hasDuplicate(allNumbers, currentNumbers) {
-  for (const num of currentNumbers) {
-    if (allNumbers.has(num)) {
-      return true; // 중복 발견
-    }
-  }
-  return false; // 중복 없음
-}
-
-generateLottoNumbers();
+generateUniqueLottoNumbers();
